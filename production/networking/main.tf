1a8e7e4510056a7a5958c7efc62cad64febd13a0
Vladimir <runonautomation@gmail.com>
2019-06-09 09:39:57 +0300

    Remove unneeded resource bucket
    Remove unneeded storage bucket

--- production/networking/main.tf
+++ production/networking/main.tf
@@ -34,9 +34,3 @@ resource "google_compute_subnetwork" "subnet_a1" {
   }
   private_ip_google_access = "true"
 }
-
-resource "google_storage_bucket" "testgitops" {
-  name          = "testgitops0123445677"
-  location      = "EU"
-  project       = "${local.project}"
-}
